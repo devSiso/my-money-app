15353617e6d025f446d46d83c5bcd6ca12389996
Matheus Cruz <Matheus@MacMini-Matheus-Cruz.local>
2019-02-07 13:36:54 -0200

    melhorias

--- frontend/src/common/template/header.jsx
+++ frontend/src/common/template/header.jsx
@@ -4,10 +4,10 @@ export default props => (
     <header className="main-header">
         <a href="/" className="logo">
             <span className="logo-mini">
-                <b> My</b> M
+                <i className="fa fa-money"/>
             </span>
             <span className="logo-lg">
-                <i className="fa fa-money"></i>
+                <i className="fa fa-money"/>
                 <b> My</b> Money
             </span>
         </a>
